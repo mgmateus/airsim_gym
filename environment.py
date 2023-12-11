bf750638ea2a3d378f8fd4facad0af513688c677
mgmateus <eng.mgmateus@gmail.com>
2023-12-11 09:11:47 -0300

    get package name

--- environment.py
+++ environment.py
@@ -6,5 +6,5 @@ from .airsim_simulation_resources.simulation import Ue4Briedge
 
 
 class Env(Ue4Briedge):
-    def __init__(self) -> None:
-        Ue4Briedge.__init__(self)
+    def __init__(self, package_name : str) -> None:
+        Ue4Briedge.__init__(self, package_name)
